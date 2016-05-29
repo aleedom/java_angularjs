96c5fc8d8aaef220a56b44508e09964bb77c94fb
Andrew Leedom <andrew.leedom@addteq.com>
2016-05-29 18:24:33 -0400

    initial commit

---
+++ src/test/java/com/aleedom/AngularjsApplicationTests.java
@@ -0,0 +1,18 @@
+package com.aleedom;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.SpringApplicationConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.web.WebAppConfiguration;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@SpringApplicationConfiguration(classes = AngularjsApplication.class)
+@WebAppConfiguration
+public class AngularjsApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
